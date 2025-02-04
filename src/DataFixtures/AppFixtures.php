69759179393432fc24ddc5f05779f2a7d5f871ae
Rayane BELABBAS <rayanebelabbas15@gmail.com>
2025-02-04 12:11:18 +0100

    ajoue de la fixtures création de l'admin

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $adminUser = new User();
+        $adminUser->setEmail('admin@admin.com');
+        $adminUser->setRoles(['ROLE_ADMIN']);
+        $adminUser->setPassword('admin');
+        $manager->persist($adminUser);
+
+        $manager->flush();
+    }
+}
