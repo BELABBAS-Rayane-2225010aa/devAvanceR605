0250412fcd8e9fb5eba477898fe38a4f0cbdb49c
Rayane BELABBAS <rayanebelabbas15@gmail.com>
2025-02-06 17:20:51 +0100

    bonjour vrai nom

--- src/Controller/QcmController.php
+++ src/Controller/QcmController.php
@@ -5,14 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Security;
 
 final class QcmController extends AbstractController
 {
+
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     #[Route('/qcm', name: 'app_qcm')]
     public function index(): Response
     {
+        $user = $this->security->getUser();
+        $username = $user ? $user->getEmail() : 'Guest';
+
         return $this->render('qcm/index.html.twig', [
             'controller_name' => 'QcmController',
+            'username' => $username,
         ]);
     }
 }
