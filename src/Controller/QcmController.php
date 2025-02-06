c4eae0a1dc24199a1866e116376e96e079f809e9
Adam <adam290604.fr@gmail.com>
2025-02-06 17:15:53 +0100

    Qcm and qcmDetails

--- src/Controller/QcmController.php
+++ src/Controller/QcmController.php
@@ -2,17 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Qcm;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class QcmController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/qcm', name: 'app_qcm')]
     public function index(): Response
     {
+        $qcms = $this->entityManager->getRepository(Qcm::class)->findAll();
+
         return $this->render('qcm/index.html.twig', [
-            'controller_name' => 'QcmController',
+            'qcms' => $qcms,
+        ]);
+    }
+
+    #[Route('/qcm/{id}', name: 'app_qcm_show')]
+    public function show(int $id): Response
+    {
+        $qcm = $this->entityManager->getRepository(Qcm::class)->find($id);
+
+        if (!$qcm) {
+            throw $this->createNotFoundException('The QCM does not exist');
+        }
+
+        return $this->render('qcm/show.html.twig', [
+            'qcm' => $qcm,
         ]);
     }
 }
