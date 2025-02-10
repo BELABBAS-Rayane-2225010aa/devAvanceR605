7aee83baefc6ed699a1096b8bf63398c931c5e3e
Adam <adam290604.fr@gmail.com>
2025-02-10 09:05:27 +0100

    Affichage des qcm et leurs contenu + fixture de test

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Qcm;
+use App\Entity\Question;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -14,7 +16,31 @@ class AppFixtures extends Fixture
         $adminUser->setEmail('admin@admin.com');
         $adminUser->setRoles(['ROLE_ADMIN']);
         $adminUser->setPassword('$2y$13$ePnGgMqJyjCL6Z/03nIONe.u4DT5KWebo0lrwr1hry1X23r6RwWI2');
+
         $manager->persist($adminUser);
+
+        $qcm = new Qcm();
+        $qcm->setTheme('Maths');
+        $qcm->setName('Maths 1');
+        $question1 = new Question();
+        $question1->setQustionstr('1 + 1 = ?');
+        $question1->setType(\Suit::CU);
+        $question2 = new Question();
+        $question2->setQustionstr('2 + 2 = ?');
+        $question2->setType(\Suit::CU);
+        $question3 = new Question();
+        $question3->setQustionstr('3 + 3 = ?');
+        $question3->setType(\Suit::CU);
+        $qcm->addQuestion($question1);
+        $qcm->addQuestion($question2);
+        $qcm->addQuestion($question3);
+
+        $manager->persist($question1);
+        $manager->persist($question2);
+        $manager->persist($question3);
+        $manager->persist($qcm);
+
+
 
         $manager->flush();
     }
