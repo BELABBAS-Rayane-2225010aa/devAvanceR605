4d5132dc31b4470bb867be77a266d70c6eda60ac
Rayane BELABBAS <rayanebelabbas15@gmail.com>
2025-02-13 14:42:51 +0100

    fix register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -32,7 +32,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_qcm');
+            return $this->redirectToRoute('app_connection');
         }
 
         return $this->render('registration/register.html.twig', [
